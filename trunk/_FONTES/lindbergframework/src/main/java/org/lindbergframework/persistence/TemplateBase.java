913fde66507276d1c18c9afd7adbeb7b3bdb9dbe
grebdnnil@hotmail.com <grebdnnil@hotmail.com@942a213e-5dd6-11de-8dc1-e798e446b60a>
2010-10-14 19:05:00 +0000

    - javadoc added

--- trunk/_FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/TemplateBase.java
+++ trunk/_FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/TemplateBase.java
@@ -1,6 +1,7 @@
 package org.lindbergframework.persistence;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,17 +17,36 @@ import org.lindbergframework.persistence.sql.DataSet;
 import org.lindbergframework.persistence.sql.SqlCommandResolver;
 
 /**
- * 
+ * abstract template base with main resources for all persistence template implementations.
+ *   
  * @author Victor Lindberg
  *
  */
 public abstract class TemplateBase implements PersistenceTemplate {
 	
+    /**
+     * bean populator implementation to use.
+     */
 	private BeanPopulator beanPopulator;
+	
+	/**
+	 * data source implementation to use.
+	 */
 	private DataSource dataSource;
+	
+	/**
+	 * data source configuration to use.
+	 */
 	private DataSourceConfig dataSourceConfig;
+	
+	/**
+	 * persistence context instance.
+	 */
 	protected LinpContext context = LinpContext.getInstance();
 	
+	/**
+	 * Default Constructor that configures default bean populator and data source implementation.
+	 */
 	public TemplateBase(){
 		configureDefaultPopulator();
 		configureDefaultDataSource();
@@ -42,15 +62,29 @@ public abstract class TemplateBase implements PersistenceTemplate {
 		configureDefaultPopulator();
 	}
 	
+	public TemplateBase(BeanPopulator populator, DataSourceConfig dsConfig){
+        setBeanPopulator(populator);
+        setDataSourceConfig(dsConfig);
+    }
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public void configureDefaultPopulator(){
 		setBeanPopulator((BeanPopulator) LindbergPersistenceSpringBeanFactory.getInstance()
 		               .getBean(PersistBeans.MULT_LEVEL_BEAN_POPULATOR));
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void configureDefaultDataSource(){
 		setDataSourceConfig(context.getDataSourceConfig());
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setDataSourceConfig(DataSourceConfig dataSourceConfig) {
 	   if (dataSourceConfig == null)
 		   throw new PersistenceException("DataSourceConfig can not to be null");
@@ -59,16 +93,24 @@ public abstract class TemplateBase implements PersistenceTemplate {
 	   this.dataSource = dataSourceConfig.getDataSource();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public DataSourceConfig getDataSourceConfig() {
 		return dataSourceConfig;
 	}
-	
-	public TemplateBase(BeanPopulator populator, DataSourceConfig dsConfig){
-		setBeanPopulator(populator);
-		setDataSourceConfig(dsConfig);
-	}
-	
-	
+
+	/**
+	 * Populates the {@link DataSet} result as a list of beans using the configured bean populator
+	 * implementation in this template.
+	 * 
+	 * @param <E> expected bean type.
+	 * @param clazz bean class for population.
+	 * @param ds {@link DataSet} implementation to populate.
+	 * @return populated beans list.
+	 * @throws SQLException error access {@link DataSet}.
+	 * @throws BeanPopulateException population failed.
+	 */
 	public <E> List<E> populateBeans(Class<E> clazz, DataSet ds) throws SQLException, BeanPopulateException{
 		ds.beforeFirst();
 		List<E> list = new ArrayList<E>();
@@ -81,37 +123,65 @@ public abstract class TemplateBase implements PersistenceTemplate {
 		return list;
 	}
 	
+	/**
+	 * Populates a bean for unique result. Only first record from {@link DataSet} will be populated.
+	 * 
+	 * @param <E> expected bean type.
+     * @param clazz bean class for population.
+     * @param ds {@link DataSet} implementation to populate.
+     * @return populated bean.
+     * @throws SQLException error access {@link DataSet}.
+     * @throws BeanPopulateException population failed.
+	 */
 	public <E> E populateBean(Class<E> clazz,DataSet ds) throws SQLException, BeanPopulateException{
 		ds.first();
 		
 		return beanPopulator.populate(clazz, ds);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setBeanPopulator(BeanPopulator beanPopulator) {
 		this.beanPopulator = beanPopulator;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public BeanPopulator getBeanPopulator() {
 		return beanPopulator;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public DataSource getDataSource() {
 		return dataSource;
 	}
 	
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public Connection getConnection() throws SQLException{
 	   verifyDataSource();
 		   
 	   return getDataSource().getConnection();
 	}
 	
+	/**
+	 * Checks if the data source is configured.
+	 */
 	protected void verifyDataSource(){
 		if (getDataSource() == null)
 			throw new IllegalStateException("No DataSource defined. Use LinpContext class to configure the default context persistence parameters");
 	}
 	
-	protected SqlCommandResolver getSqlCommandResolver(){
+	/**
+	 * {@inheritDoc}
+	 */
+	public SqlCommandResolver getSqlCommandResolver(){
 		return context.getSqlCommandResolver();
 	}
 	
