30c3c72c379be5ba19d9a792f3ab69e6bfc558fc
grebdnnil <grebdnnil@hotmail.com>
2010-10-14 21:42:28 +0000

    - javadoc added

--- _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/beans/BeanPopulator.java
+++ _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/beans/BeanPopulator.java
@@ -6,13 +6,24 @@ import org.lindbergframework.exception.BeanPopulateException;
 import org.lindbergframework.persistence.sql.DataSet;
 
 /**
- * 
+ * Interface that defines the bean populator implementations contract.
+ *  
  * @author Victor Lindberg
  *
  */
 public interface BeanPopulator {
 
-	public <E> E populate(Class<E> bean, DataSet dataSet) 
+    /**
+     * Creates and Populates a bean based on {@link DataSet} argument.
+     * 
+     * @param <E> bean type.
+     * @param beanClass bean class to populate.
+     * @param dataSet {@link DataSet} that will be used to populate the bean.
+     * @return populated bean.
+     * @throws SQLException accessing error {@link DataSet}.
+     * @throws BeanPopulateException bean population failed.
+     */
+	public <E> E populate(Class<E> beanClass, DataSet dataSet) 
 	 						throws SQLException, BeanPopulateException;
 	
 }
--- _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/beans/MultLevelBeanRowMapperForProcedureCursor.java
+++ _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/beans/MultLevelBeanRowMapperForProcedureCursor.java
@@ -6,16 +6,20 @@ import java.sql.SQLException;
 import org.lindbergframework.exception.BeanPopulateException;
 import org.lindbergframework.persistence.sql.ResultDataSet;
 import org.lindbergframework.persistence.sql.RowDataTree;
-import org.lindbergframework.persistence.translate.SqlParametersTranslator;
 import org.springframework.jdbc.core.RowMapper;
 
 /**
+ * MultLevelsBeanPopulator extension that implements {@link RowMapper} interface
+ * to work with Procedure and Function cursors.
  * 
  * @author Victor Lindberg
  *
  */
 public class MultLevelBeanRowMapperForProcedureCursor extends MultLevelsBeanPopulator implements RowMapper{
 	
+    /**
+     * Bean class to populate.
+     */
 	private Class clazz;
 	
 	public MultLevelBeanRowMapperForProcedureCursor(Class clazz) {
@@ -23,6 +27,10 @@ public class MultLevelBeanRowMapperForProcedureCursor extends MultLevelsBeanPopu
 	}
 
 
+	/**
+	 * Executes the mult level population for {@link ResultSet} argument
+	 * creating and populating the bean.
+	 */
 	public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
 		try {
 			ResultDataSet dataSet = new ResultDataSet(rs);
