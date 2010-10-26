9f70b1fae722557dd8ccbd8626efefe6eb068e82
grebdnnil <grebdnnil@hotmail.com>
2010-10-26 13:37:24 +0000

    - javadoc added

---
+++ _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/context/LinpConfigurationInitializer.java
@@ -0,0 +1,35 @@
+package org.lindbergframework.persistence.context;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Interface that defines a linp configuration initializer.
+ * 
+ * @author Victor Lindberg
+ *
+ */
+public interface LinpConfigurationInitializer {
+    
+    /**
+     * Initialize the linp configurations using {@link File}.
+     * 
+     * @param xmlConfigFiles configuration Files.
+     */
+    public void initialize(File... xmlConfigFiles);
+     
+    /**
+     * Initialize the linp configuration using {@link URL}.
+     * 
+     * @param xmlConfigURLs configuration URLs.
+     */
+    public void initialize(URL... xmlConfigURLs);
+     
+    /**
+     * Initialize the linp configuration using {@link InputStream}.
+     * 
+     * @param xmlConfigInputStreams configuration InputStreams.
+     */
+    public void initialize(InputStream... xmlConfigInputStreams);
+}
--- _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/sql/XmlSqlCommandResolver.java
+++ _FONTES/lindbergframework/src/main/java/org/lindbergframework/persistence/sql/XmlSqlCommandResolver.java
@@ -7,13 +7,17 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.smartcardio.CommandAPDU;
+
 import org.apache.xmlbeans.XmlException;
+import org.lindbergframework.core.context.ConfigurationInitializer;
 import org.lindbergframework.exception.DuplicatedSqlMappingIdException;
 import org.lindbergframework.exception.SqlCommandLoadingFailedException;
 import org.lindbergframework.exception.InvalidSqlMappingException;
 import org.lindbergframework.exception.NoSuchSqlCommandException;
 import org.lindbergframework.exception.NoSuchSqlStatementException;
 import org.lindbergframework.exception.SqlMappingException;
+import org.lindbergframework.persistence.context.LinpConfigurationInitializer;
 import org.lindbergframework.schema.LinpMappingDocument;
 import org.lindbergframework.schema.TFunction;
 import org.lindbergframework.schema.TFunctionResultCursor;
@@ -24,6 +28,7 @@ import org.lindbergframework.schema.LinpMappingDocument.LinpMapping;
 import org.springframework.util.StringUtils;
 
 /**
+ * Sql command resolver implementation to work with XML mapping for sql commands.
  * 
  * The XML schema  is defined in http://www.lindbergframework.org/schema/linp-sqlMapping.xsd
  * 
@@ -31,38 +36,70 @@ import org.springframework.util.StringUtils;
  *
  */
 
-public class XmlSqlCommandResolver implements SqlCommandResolver{
+public class XmlSqlCommandResolver implements SqlCommandResolver, LinpConfigurationInitializer{
 
+    /**
+     * lindberg persistence mapping files nodes.
+     */
 	private LinpMapping[] linpMappings;
 	
 	public XmlSqlCommandResolver(){
 		//
 	}
 	
+	/**
+	 * creates a XmlSqlCommandResolver for xml Files.
+	 * 
+	 * @param xmlSqlCommandLocations mapping files.
+	 */
 	public XmlSqlCommandResolver(File... xmlSqlCommandLocations){
 		load(xmlSqlCommandLocations);
 	}
 	
+	/**
+	 * creates a XmlSqlCommandResolver for xml URLs.
+	 * 
+	 * @param xmlSqlCommandLocations mapping URLs.
+	 */
 	public XmlSqlCommandResolver(URL... xmlSqlCommandLocations){
 		load(xmlSqlCommandLocations);
 	}
 	
+	/**
+	 * creates a XmlSqlCommandResolver for xml inputStreams.
+	 * 
+	 * @param xmlSqlCommandLocations mapping inputStreams.
+	 */
 	public XmlSqlCommandResolver(InputStream... xmlSqlCommandLocations){
 		load(xmlSqlCommandLocations);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void initialize(File... xmlSqlCommandLocations){
 		load(xmlSqlCommandLocations);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void initialize(URL... xmlSqlCommandLocations){
 		load(xmlSqlCommandLocations);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void initialize(InputStream... xmlSqlCommandLocations){
 		load(xmlSqlCommandLocations);
 	}
 	
+	/**
+	 * loads the configuration objects in this command resolver.
+	 * 
+	 * @param xmlSqlCommandLocations configuration objects.
+	 */
 	private void load(Object[] xmlSqlCommandLocations){
 		if (linpMappings != null)
 			   throw new IllegalStateException("Sql Mapping context is already initialized");
@@ -76,6 +113,9 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		validate();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public SqlStatement getSqlStatement(String id) throws NoSuchSqlCommandException {
 		TSqlCommand command = getTSqlCommand(id);
 		String statement = command.getSqlStatement();
@@ -90,6 +130,9 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		return sqlStatement;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public SqlProcedure getSqlProcedure(String id) throws NoSuchSqlCommandException {
 		TSqlCommand command = getTSqlCommand(id);
 		TProcedure tProcedure = command.getProcedure();
@@ -108,6 +151,9 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		return procedure;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public SqlFunction getSqlFunction(String id) throws NoSuchSqlCommandException{
 		TSqlCommand command = getTSqlCommand(id);
 		TFunction tFunction = command.getFunction();
@@ -129,6 +175,12 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		return function;
 	}
 	
+	/**
+	 * loads a {@link SqlFunction} based on {@link TFunction} xml node.
+	 * 
+	 * @param sqlFunction {@link SqlFunction} to load.
+	 * @param tFunction {@link TFunction} to load the {@link SqlFunction}.
+	 */
 	public void loadSqlFunction(SqlFunction sqlFunction,TFunction tFunction){
 		sqlFunction.setCatalog(tFunction.getCatalog());
 		sqlFunction.setSchema(tFunction.getSchema());
@@ -148,8 +200,13 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 			((SqlFunctionForCursor) sqlFunction).setBeanResult(toClass(className, sqlFunction.getId()));
 		}
 	}
-	
-	
+
+	/**
+	 * loads the output cursor parameters on {@link SqlProcedure} based on {@link TOutCursor} xml node array.
+	 * 
+	 * @param procedure {@link SqlProcedure} to load.
+	 * @param outCursors {@link TOutCursor} output cursor array to load the procedure.
+	 */
 	protected void loadSqlOutCursorParams(SqlProcedure procedure, TOutCursor[] outCursors){
   	       for (TOutCursor outCursor : outCursors){
 		      String className = outCursor.getClass1();
@@ -158,7 +215,15 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		   }
 	}
 	
-	private Class toClass(String className, String id){
+	/**
+	 * Extracts the {@link Class} based on className argument.
+	 * @param className className to extract.
+	 * @param id id of current sql command.
+	 * 
+	 * @return class instance.
+	 * @throws SqlCommandLoadingFailedException error getting class.
+	 */
+	private Class toClass(String className, String id) throws SqlCommandLoadingFailedException{
 		try{
 		   return Class.forName(className);	
 		}catch (ClassNotFoundException ex) {
@@ -166,6 +231,14 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		}
 	}
 	
+	/**
+	 * Get the {@link TSqlCommand} node associated with the specified id.
+	 * 
+	 * @param id {@link SqlCommand} id.
+	 * @return {@link TSqlCommand} associated with the specified id.
+	 * 
+	 * @throws NoSuchSqlCommandException sql command for specified id not found.
+	 */
 	protected TSqlCommand getTSqlCommand(String id) throws NoSuchSqlCommandException{
 		TSqlCommand command = null;
 		for (LinpMapping mapping : linpMappings){
@@ -180,6 +253,14 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		return command;
 	}
 	
+	/**
+	 * Get the {@link SqlCommand} associated with specified id in {@link LinpMapping} node.
+	 * 
+	 * @param mapping linp xml node to search.
+	 * @param id sql command id.
+	 * @return {@link TSqlCommand} associated with the specified id or null
+	 * if a {@link TSqlCommand} with the specified id not found.
+	 */
 	protected TSqlCommand getTSqlCommand(LinpMapping mapping,String id){
 		TSqlCommand[] commandArray = mapping.getSqlMapping().getSqlCommandArray();
 		for (TSqlCommand command : commandArray)
@@ -189,6 +270,12 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		return null;
 	}
 	
+	/**
+	 * loads the document getting {@link LinpMapping} node.
+	 * 
+	 * @param sqlMapping sql mapping object.
+	 * @return {@link LinpMapping} node.
+	 */
 	private LinpMapping loadDocument(Object sqlMapping) {
 		if (sqlMapping == null)
 			throw new SqlMappingException(
@@ -201,6 +288,13 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		}
 	}
 	
+	/**
+	 * Builds the {@link LinpMapping} node based on sql mapping object argument.
+	 * @param sqlMapping sql mapping object.
+	 * @return {@link LinpMapping} node.
+	 * @throws IOException Error getting {@link LinpMapping} node.
+	 * @throws XmlException XML mapping invalid.
+	 */
 	private LinpMapping buildDocument(Object sqlMapping) throws IOException, XmlException {
 		LinpMappingDocument doc = null;
 		
@@ -225,11 +319,19 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
 		return doc.getLinpMapping();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void validate() throws InvalidSqlMappingException {
 		validateConfigurationDocument(linpMappings);
 		verifyUniqueIDs(linpMappings);	
 	}
 	
+	/**
+	 * validates the sql mappings.
+	 * 
+	 * @param linpMappings sql mappings to validate.
+	 */
     private void validateConfigurationDocument(LinpMapping[] linpMappings){
        for (LinpMapping linpMapping : linpMappings){
           if (! linpMapping.validate())
@@ -237,20 +339,26 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
        }
     }
     
+    /**
+     * checks duplicated ID´s in sql mappings. A ID must be unique in all context. 
+     * 
+     * @param linpMappings sql mappings.
+     * @throws InvalidSqlMappingException invalid sql mapping.
+     */
     private void verifyUniqueIDs(LinpMapping[] linpMappings) throws InvalidSqlMappingException{
     	if (linpMappings == null)
     		throw new InvalidSqlMappingException("Linp sql mappings is null");
     	
     	Set<String> ids = new HashSet<String>();
-    	Set<String> idsThrowed = new HashSet<String>();
+    	Set<String> duplicatedIDs = new HashSet<String>();
     	DuplicatedSqlMappingIdException ex = new DuplicatedSqlMappingIdException();
     	for (LinpMapping mapping : linpMappings){
     		TSqlCommand[] commands = mapping.getSqlMapping().getSqlCommandArray();
     		for (TSqlCommand command : commands){
     			String id = command.getId();
-    			if (ids.contains(id) && ! idsThrowed.contains(id)){
+    			if (ids.contains(id) && ! duplicatedIDs.contains(id)){
     				ex.addMessage("Duplicated Sql Command ID : "+id);
-    				idsThrowed.add(id);
+    				duplicatedIDs.add(id);
     			}
     			ids.add(id);
     		}
@@ -259,6 +367,13 @@ public class XmlSqlCommandResolver implements SqlCommandResolver{
     	ex.throwIfContainsErrorMessages();
     }
     
+    /**
+     * clean the string value of a {@link SqlCommand}.
+     * 
+     * @param strCommand string value of {@link SqlCommand}.
+     * 
+     * @return string cleaned.
+     */
     private String cleanStringCommand(String strCommand){
     	strCommand = StringUtils.delete(strCommand, "\n");
     	
