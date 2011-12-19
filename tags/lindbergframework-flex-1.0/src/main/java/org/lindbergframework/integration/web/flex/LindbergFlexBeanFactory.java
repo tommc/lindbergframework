ec1e09629f81809967bcca9d8ad72b5f0ae5e952
grebdnnil@hotmail.com <grebdnnil@hotmail.com@942a213e-5dd6-11de-8dc1-e798e446b60a>
2011-12-19 19:06:44 +0000

    [maven-release-plugin]  copy for tag lindbergframework-flex-1.0

---
+++ tags/lindbergframework-flex-1.0/src/main/java/org/lindbergframework/integration/web/flex/LindbergFlexBeanFactory.java
@@ -0,0 +1,49 @@
+package org.lindbergframework.integration.web.flex;
+
+import org.lindbergframework.beans.di.context.BeanFactory;
+import org.lindbergframework.beans.di.context.UserBeanContext;
+
+import flex.messaging.FactoryInstance;
+import flex.messaging.FlexFactory;
+import flex.messaging.config.ConfigMap;
+
+/**
+ * Lindberg flex factory implementation to integrate
+ * the flex layer with a business layer that uses lindbergframework IOC.
+ *   
+ * @author Victor Lindberg
+ *
+ */
+public class LindbergFlexBeanFactory implements FlexFactory{
+    
+    private BeanFactory beanFactory;
+    
+    public LindbergFlexBeanFactory(){
+        //
+    }
+    
+    /**
+     * Initializes the lindberg bean factory defined in core configuration from {@link UserBeanContext}. 
+     */
+    public void initialize(String id, ConfigMap configMap){
+        beanFactory = UserBeanContext.getInstance().getFactory();
+    }
+
+    /**
+     * Creates and configures Lindberg {@link FactoryInstance} implementation. 
+     */
+    public FactoryInstance createFactoryInstance(String id, ConfigMap properties){
+        LindbergFlexFactoryInstance factoryInstance = new LindbergFlexFactoryInstance(this, id, properties,beanFactory);
+        factoryInstance.setSource(properties.getPropertyAsString(SOURCE, factoryInstance.getId()));
+        return factoryInstance;
+    }
+
+    /**
+     * Get the bean from lindberg IOC context.
+     */
+    public Object lookup(FactoryInstance inst){
+        LindbergFlexFactoryInstance factoryInstance = (LindbergFlexFactoryInstance) inst;
+        return factoryInstance.lookup(); 
+    }
+    
+}
