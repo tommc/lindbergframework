07a61a30d357b5da0f1c1add9d359050ac6bd621
grebdnnil@hotmail.com <grebdnnil@hotmail.com@942a213e-5dd6-11de-8dc1-e798e446b60a>
2011-12-24 05:22:07 +0000

    [maven-release-plugin]  copy for tag lindbergframework-xmlsqlmapping-1.0

---
+++ tags/lindbergframework-xmlsqlmapping-1.0/src/main/java/org/lindbergframework/schema/impl/TOutCursorImpl.java
@@ -0,0 +1,94 @@
+/*
+ * XML Type:  tOutCursor
+ * Namespace: http://www.lindbergframework.org/schema
+ * Java type: org.lindbergframework.schema.TOutCursor
+ *
+ * Automatically generated - do not modify.
+ */
+package org.lindbergframework.schema.impl;
+/**
+ * An XML tOutCursor(@http://www.lindbergframework.org/schema).
+ *
+ * This is a complex type.
+ */
+public class TOutCursorImpl extends org.apache.xmlbeans.impl.values.XmlComplexContentImpl implements org.lindbergframework.schema.TOutCursor
+{
+    private static final long serialVersionUID = 1L;
+    
+    public TOutCursorImpl(org.apache.xmlbeans.SchemaType sType)
+    {
+        super(sType);
+    }
+    
+    private static final javax.xml.namespace.QName POPULATIONCLASS$0 = 
+        new javax.xml.namespace.QName("", "population-class");
+    
+    
+    /**
+     * Gets the "population-class" attribute
+     */
+    public java.lang.String getPopulationClass()
+    {
+        synchronized (monitor())
+        {
+            check_orphaned();
+            org.apache.xmlbeans.SimpleValue target = null;
+            target = (org.apache.xmlbeans.SimpleValue)get_store().find_attribute_user(POPULATIONCLASS$0);
+            if (target == null)
+            {
+                return null;
+            }
+            return target.getStringValue();
+        }
+    }
+    
+    /**
+     * Gets (as xml) the "population-class" attribute
+     */
+    public org.apache.xmlbeans.XmlString xgetPopulationClass()
+    {
+        synchronized (monitor())
+        {
+            check_orphaned();
+            org.apache.xmlbeans.XmlString target = null;
+            target = (org.apache.xmlbeans.XmlString)get_store().find_attribute_user(POPULATIONCLASS$0);
+            return target;
+        }
+    }
+    
+    /**
+     * Sets the "population-class" attribute
+     */
+    public void setPopulationClass(java.lang.String populationClass)
+    {
+        synchronized (monitor())
+        {
+            check_orphaned();
+            org.apache.xmlbeans.SimpleValue target = null;
+            target = (org.apache.xmlbeans.SimpleValue)get_store().find_attribute_user(POPULATIONCLASS$0);
+            if (target == null)
+            {
+                target = (org.apache.xmlbeans.SimpleValue)get_store().add_attribute_user(POPULATIONCLASS$0);
+            }
+            target.setStringValue(populationClass);
+        }
+    }
+    
+    /**
+     * Sets (as xml) the "population-class" attribute
+     */
+    public void xsetPopulationClass(org.apache.xmlbeans.XmlString populationClass)
+    {
+        synchronized (monitor())
+        {
+            check_orphaned();
+            org.apache.xmlbeans.XmlString target = null;
+            target = (org.apache.xmlbeans.XmlString)get_store().find_attribute_user(POPULATIONCLASS$0);
+            if (target == null)
+            {
+                target = (org.apache.xmlbeans.XmlString)get_store().add_attribute_user(POPULATIONCLASS$0);
+            }
+            target.set(populationClass);
+        }
+    }
+}
