bcc818fe48fb554ad189f01bfdbd2fb92b686da2
victorlindberg713@gmail.com <victorlindberg713@gmail.com@942a213e-5dd6-11de-8dc1-e798e446b60a>
2012-05-05 21:47:24 +0000

    [maven-release-plugin]  copy for tag lindbergframework-myview-1.0

---
+++ tags/lindbergframework-myview-1.0/src/main/java/org/lindbergframework/orm/hibernate/support/MyViewScopesOpenSessionInViewFilter.java
@@ -0,0 +1,22 @@
+package org.lindbergframework.orm.hibernate.support;
+
+import java.util.logging.Logger;
+
+import org.hibernate.FlushMode;
+import org.springframework.orm.hibernate3.support.OpenSessionInViewFilter;
+
+/**
+ * {@link OpenSessionInViewFilter} extension that works with FlushMode.AUTO by default.
+ * 
+ * @author Victor Lindberg
+ *
+ */
+public class MyViewScopesOpenSessionInViewFilter extends OpenSessionInViewFilter{
+
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	public MyViewScopesOpenSessionInViewFilter() {
+		setFlushMode(FlushMode.AUTO);
+		logger.info("Usando [MyViewScopesOpenSessionInViewFilter] como extensão de OpenSessionInViewFilter com FlushMode.AUTO.");
+	}
+}
